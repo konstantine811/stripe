5998ffc07ac75be6293a7115ee9e1ee65a29e155
konstantine811 <galystyan777@gmail.com>
2018-01-05 05:28:56 +0300

    deployed project and start working on first header and first section

---
+++ app/code/main.js
@@ -0,0 +1,16 @@
+/*
+* @Author: Admin
+* @Date:   2017-07-19 19:43:56
+* @Last Modified by:   Admin
+* @Last Modified time: 2017-07-20 15:28:53
+*/
+
+'use strict';
+
+$(document).ready(function() {
+
+	//bootstrap carousel
+	$('.carousel').carousel();
+});
+
+
---
+++ gulpfile.js
@@ -0,0 +1,139 @@
+var gulp = require('gulp'),
+	sass = require('gulp-sass'),
+	browserSync = require('browser-sync').create(),
+	babel = require('gulp-babel'),
+	prefixer = require('gulp-autoprefixer'),
+	useref = require('gulp-useref'),
+	gulpif = require('gulp-if'),
+	uglify = require('gulp-uglify'),
+	minifyCss = require('gulp-csso'),
+	imagemin = require('gulp-imagemin'),
+	pngquant      = require('imagemin-pngquant'),
+    cache         = require('gulp-cache'),
+    jade = require('gulp-jade');
+
+var reload      = browserSync.reload;
+
+// gulp.task('es6', function() {
+// 	return gulp.src('./app/src/**/*.js')
+// 		.pipe(babel({
+// 			presets: ['es2015']
+// 		}))
+// 		.pipe(gulp.dest('./app/code/'))
+// 		.pipe(browserSync.reload({
+// 			stream: true
+// 		}));
+// });
+// 
+
+
+//Jade task
+//
+
+function log(error) {
+    console.log([
+        '',
+        "----------ERROR MESSAGE START----------",
+        ("[" + error.name + " in " + error.plugin + "]"),
+        error.message,
+        "----------ERROR MESSAGE END----------",
+        ''
+    ].join('\n'));
+    this.end();
+}
+
+gulp.task('jade', function() {
+	return gulp.src(['./app/_jadefiles/index.jade'])
+	.pipe(jade( {
+		pretty: true
+	}).on('error', log))
+	.pipe(gulp.dest('./app'));
+});
+
+
+//Sass compile
+gulp.task('sass', function() {
+	return gulp.src('./app/sass/**/*.+(scss|sass)')
+		.pipe(sass().on('error', sass.logError))
+		.pipe(prefixer({
+				browsers: ['last 15 versions', '> 1%', 'ie 8', 'ie 7']
+			}))
+		.pipe(gulp.dest('app/style/'))
+		.pipe(browserSync.reload({
+			stream: true
+		}));
+});
+
+gulp.task('clear', function() {
+    return cache.clearAll();
+});
+
+
+//mini img
+gulp.task('img', function() {
+    return gulp.src('app/img/**/*')
+    .pipe(cache(imagemin({
+        interlaced: true,
+        progressive: true,
+        pngquant: true,
+		optipng: false,
+		zopflipng: true,
+		jpegRecompress: false,
+		jpegoptim: true,
+		mozjpeg: true,
+		gifsicle: true,
+		concurrent: 10,
+        svgoPlugin: [{removeViewBox: false}],
+        une: [pngquant()]
+    })))
+    .pipe(gulp.dest('public/img'));
+});
+
+
+
+
+
+gulp.task('build', ['img'], function() {
+	return gulp.src('app/*.html')
+		.pipe(useref())
+		.pipe(gulpif('*.js', uglify()))
+		.pipe(gulpif('*.css', minifyCss()))
+		.pipe(gulp.dest('public'))
+});
+
+
+
+
+gulp.task('browserSync', ['sass', 'jade'], function() {
+	browserSync.init({
+		server: { baseDir: './app'},
+		notify: false,
+		ghostMode: {
+          clicks: true,
+          location: true,
+          forms: true,
+          scroll: false
+      }
+	});
+});
+
+gulp.task('jade-rebuild', ['pug'], function() {
+	browserSync.reload();
+});
+
+gulp.task('html', function(){
+  gulp.src('./app/index.html')
+  .pipe(reload({stream:true}));
+});
+
+gulp.task('watch', function() {
+	gulp.watch('./app/sass/**/*.+(scss|sass)', ['sass']);
+	gulp.watch('./app/_jadefiles/**/*.jade', ['jade']); 
+	gulp.watch('./app/code/**/*.js', browserSync.reload);
+	 gulp.watch("./app/*.html").on('change', browserSync.reload);
+	// gulp.watch('./app/src/**/*.js', ['es6']);
+});
+
+gulp.task('default', ['browserSync','sass', 'jade', 'watch']);
+
+
