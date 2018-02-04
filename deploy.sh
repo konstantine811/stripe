73bdc40b7af6e43727754f38626562e36b2f7ae9
Vahid Kowsari <vahid@oncoverse.com>
2018-02-03 18:03:21 -0800

    Fixed case for image filenames and added a deploy script

---
+++ deploy.sh
@@ -0,0 +1,6 @@
+aws s3 sync app/ s3://ixlayer.com-new/
+aws s3 cp app/contact-us.html s3://ixlayer.com-new/contact-us --content-type "text/html"
+aws s3 cp app/about-us.html s3://ixlayer.com-new/about-us --content-type "text/html"
+aws s3 cp app/ix360.html s3://ixlayer.com-new/ix360 --content-type "text/html"
+aws s3 cp app/ixAnypoint.html s3://ixlayer.com-new/ixAnypoint --content-type "text/html"
+aws s3 cp app/sign-in.html s3://ixlayer.com-new/sign-in --content-type "text/html"
