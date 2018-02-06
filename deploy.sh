cd6f8327ac6c7bed2562159969c4b73e617429e0
Vahid Kowsari <vahid@oncoverse.com>
2018-02-06 10:11:01 -0800

    Added command to push invalidation to cloudfront

--- deploy.sh
+++ deploy.sh
@@ -6,3 +6,5 @@ aws s3 cp app/about-us.html s3://ixlayer.com-new/about-us --content-type "text/h
 aws s3 cp app/ix360.html s3://ixlayer.com-new/ix360 --content-type "text/html"
 aws s3 cp app/ixAnypoint.html s3://ixlayer.com-new/ixAnypoint --content-type "text/html"
 aws s3 cp app/sign-in.html s3://ixlayer.com-new/sign-in --content-type "text/html"
+
+#aws cloudfront create-invalidation --paths '/*' --distribution-id E3857AR2FW9R6A
