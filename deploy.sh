aee5b7547500d4db7b94918017f66756ff876d2e
Vahid Kowsari <vahid@oncoverse.com>
2018-02-05 16:47:00 -0800

    Updated deply script to deal with correcting the URLs

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,5 @@
+find app -name "*.html" -d 1 | xargs  sed -i '' -e 's|href="\(.*\).html"|href="\1"|g'
+
 aws s3 sync app/ s3://ixlayer.com-new/
 aws s3 cp app/contact-us.html s3://ixlayer.com-new/contact-us --content-type "text/html"
 aws s3 cp app/about-us.html s3://ixlayer.com-new/about-us --content-type "text/html"
