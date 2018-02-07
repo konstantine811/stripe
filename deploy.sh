fccfafeca6194f293b48d5f4504d44da22841de7
Vahid Kowsari <vahid@oncoverse.com>
2018-02-07 11:17:19 -0800

    Fixed case issue with ixAnyPoint links

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ aws s3 sync app/ s3://ixlayer.com-new/
 aws s3 cp app/contact-us.html s3://ixlayer.com-new/contact-us --content-type "text/html"
 aws s3 cp app/about-us.html s3://ixlayer.com-new/about-us --content-type "text/html"
 aws s3 cp app/ix360.html s3://ixlayer.com-new/ix360 --content-type "text/html"
-aws s3 cp app/ixAnypoint.html s3://ixlayer.com-new/ixAnypoint --content-type "text/html"
+aws s3 cp app/ixanypoint.html s3://ixlayer.com-new/ixanypoint --content-type "text/html"
 aws s3 cp app/sign-in.html s3://ixlayer.com-new/sign-in --content-type "text/html"
 
-#aws cloudfront create-invalidation --paths '/*' --distribution-id E3857AR2FW9R6A
+aws cloudfront create-invalidation --paths '/*' --distribution-id E3857AR2FW9R6A
