c2d05b542d1188cca28e9d30c99f1985712e4dae
Vahid Kowsari <vahid@oncoverse.com>
2018-02-08 10:06:43 -0800

    Updated deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,18 @@
+# Script to push website content to S3/Cloudfront
+# You need to have loaded the AWS keys as environment variables before
+# running this script
+
+BUCKET=s3://ixlayer.com
+DISTRIBUTION_ID=E3857AR2FW9R6A
+
+# change all the html extensions
 find app -name "*.html" -d 1 | xargs  sed -i '' -e 's|href="\(.*\).html"|href="\1"|g'
 
-aws s3 sync app/ s3://ixlayer.com-new/
-aws s3 cp app/contact-us.html s3://ixlayer.com-new/contact-us --content-type "text/html"
-aws s3 cp app/about-us.html s3://ixlayer.com-new/about-us --content-type "text/html"
-aws s3 cp app/ix360.html s3://ixlayer.com-new/ix360 --content-type "text/html"
-aws s3 cp app/ixanypoint.html s3://ixlayer.com-new/ixanypoint --content-type "text/html"
-aws s3 cp app/sign-in.html s3://ixlayer.com-new/sign-in --content-type "text/html"
+aws s3 sync app/ $BUCKET
+aws s3 cp app/contact-us.html $BUCKET/contact-us --content-type "text/html"
+aws s3 cp app/about-us.html $BUCKET/about-us --content-type "text/html"
+aws s3 cp app/ix360.html $BUCKET/ix360 --content-type "text/html"
+aws s3 cp app/ixanypoint.html $BUCKET/ixanypoint --content-type "text/html"
+aws s3 cp app/sign-in.html $BUCKET/sign-in --content-type "text/html"
 
-aws cloudfront create-invalidation --paths '/*' --distribution-id E3857AR2FW9R6A
+aws cloudfront create-invalidation --paths '/*' --distribution-id $DISTRIBUTION_ID 
